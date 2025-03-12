33af6117ee1a55bff8a3da20c894d63b44be129b
Admin <lmht721@gmail.com>
2025-03-12 15:33:09 +0700

    CRUD Product

--- FShop.Service/Catalog/Products/IImageService.cs
+++ FShop.Service/Catalog/Products/IImageService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using FShop.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,11 @@ using System.Threading.Tasks;
 
 namespace FShop.Service.Catalog.Products
 {
-    class IImageService
+    public class IImageService : IEntityTypeConfiguration<Image>
     {
+        public void Configure(EntityTypeBuilder<Image> builder)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
